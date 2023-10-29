875e00242abe1bbfa5063d779ea2f8ad184793cd
serge0design <github@serge.design>
2023-10-29 11:07:25 +0100

    add bundles

--- src/StarterkitBundle/Resources/install/files/config/bundles.php
+++ src/StarterkitBundle/Resources/install/files/config/bundles.php
@@ -1,6 +1,42 @@
 <?php
 
+use Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle;
+use Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle;
+use Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle;
+use Pimcore\Bundle\PersonalizationBundle\PimcorePersonalizationBundle;
+use Pimcore\Bundle\GlossaryBundle\PimcoreGlossaryBundle;
+use Pimcore\Bundle\SeoBundle\PimcoreSeoBundle;
+use Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle;
+use Pimcore\Bundle\GoogleMarketingBundle\PimcoreGoogleMarketingBundle;
+use Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle;
+use Pimcore\Bundle\WebToPrintBundle\PimcoreWebToPrintBundle;
+use Pimcore\Bundle\StaticRoutesBundle\PimcoreStaticRoutesBundle;
+use Pimcore\Bundle\NewsletterBundle\PimcoreNewsletterBundle;
+use Pimcore\Bundle\WordExportBundle\PimcoreWordExportBundle;
+use Pimcore\Bundle\XliffBundle\PimcoreXliffBundle;
+use Pimcore\Bundle\FileExplorerBundle\PimcoreFileExplorerBundle;
+use Pimcore\Bundle\SystemInfoBundle\PimcoreSystemInfoBundle;
+
+use Symfony\WebpackEncoreBundle\WebpackEncoreBundle;
+use Presta\SitemapBundle\PrestaSitemapBundle;
+
 return [
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
+    PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+    PimcoreTinymceBundle::class => ['all' => true],
+    PimcoreEcommerceFrameworkBundle::class => ['all' => true],
+    PimcorePersonalizationBundle::class => ['all' => true],
+    PimcoreGlossaryBundle::class => ['all' => true],
+    PimcoreSeoBundle::class => ['all' => true],
+    PimcoreCustomReportsBundle::class => ['all' => true],
+    PimcoreGoogleMarketingBundle::class => ['all' => true],
+    PimcoreApplicationLoggerBundle::class => ['all' => true],
+    PimcoreWebToPrintBundle::class => ['all' => true],
+    PimcoreStaticRoutesBundle::class => ['all' => true],
+    PimcoreNewsletterBundle::class => ['all' => true],
+    PimcoreWordExportBundle::class => ['all' => true],
+    PimcoreXliffBundle::class => ['all' => true],
+    PimcoreFileExplorerBundle::class => ['all' => true],
+    PimcoreSystemInfoBundle::class => ['all' => true],
+    WebpackEncoreBundle::class => ['all' => true],
+    PrestaSitemapBundle::class => ['all' => true],
 ];
